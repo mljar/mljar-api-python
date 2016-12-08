9542b8c17bec8047a401b153c95a0dfb93b9910d
Piotr Plonski <piotr.plonski@iqor.com>
2016-12-08 14:20:44 +0100

    work in progress, bug in s3 upload

--- mljar/client.py
+++ mljar/client.py
@@ -6,6 +6,7 @@ import json, requests
 # pip install pyopenssl ndg-httpsclient pyasn1
 
 API_VERSION = 'v1'
+MLJAR_ENDPOINT = 'http://0.0.0.0:3000/api'
 
 class Error(Exception):
     """Base exception class for this module"""
@@ -43,23 +44,39 @@ class Client(object):
             self.API_ENDPOINT = api_endpoint
 
         if not self.API_ENDPOINT:
-            self.API_ENDPOINT = 'https://mljar.com'
+            self.API_ENDPOINT = MLJAR_ENDPOINT
 
         self._urls = {
-            'project': '/'.join([self.API_ENDPOINT, API_VERSION, 'projects'])
-        }
-        #print self._urls, self.TOKEN
+            'project':   '/'.join([self.API_ENDPOINT, API_VERSION, 'projects']),
+            's3policy':  '/'.join([self.API_ENDPOINT, API_VERSION, 's3policy/']),
 
-    def _make_request(self, url_name, request_type = 'get'):
+        }
+        print self._urls, self.TOKEN
+
+    def _make_request(self, url_name = '', custom_url = '', request_type = 'get', url_additional = '', input_json = {}, with_header = True):
         try:
             response = None
-            input_json = {}
+
             query_parameters = {}
             headers = {'Authorization': 'Token '+self.TOKEN}
+            my_url = ''
+            if url_name in self._urls:
+                my_url = self._urls[url_name]
+            if custom_url != '':
+                my_url = custom_url
+            if my_url == '':
+                raise Exception('Wrong URL address')
+
             if request_type == 'get':
-                response = requests.get(self._urls[url_name], headers=headers)
+                if url_additional != '':
+                    my_url += '/' + url_additional
+                response = requests.get(my_url, headers=headers)
+                print 'url', response.url
             elif request_type == 'post':
-                response = requests.post(self._urls[url_name], data=input_json, headers=headers, params=query_parameters)
+                if with_header:
+                    response = requests.post(my_url, data=input_json, headers=headers)
+                else:
+                    response = requests.post(my_url, data=input_json)
         except Exception as e:
             print 'There was an error during API call, %s' % str(e)
         finally:
@@ -96,4 +113,86 @@ class MljarClient(Client):
 
         print 'There are', len(projects), 'projects'
         for proj in projects:
-            print 'Project:', proj['title']
+            print 'Project title:', proj['title'], 'hid:', proj['hid']
+
+    def _task_to_full_name(self, task_short):
+        tasks = {'bin_class': "Binary classification",
+                    'reg': "Regression",
+                    'img_class': "Images classification"}
+        if task_short not in tasks:
+            return 'Unknown task'
+        return tasks[task_short]
+
+    def get_project_details(self, project_hid, verbose = True):
+        '''
+            Print out project details and return details json.
+        '''
+        print 'Get project details', project_hid
+        response = self._make_request(url_name = 'project', request_type = 'get', url_additional = project_hid)
+        print 'Response', response
+        details = self._get_data(response)
+        if verbose:
+            print '-'*50,'\nProject details\n','-'*50
+            print 'Title:', details['title']
+            if details['description']:
+                print 'Description:', details['description']
+            print 'Task:', self._task_to_full_name(details['task'])
+            print 'Hardware:', details['hardware']
+            print 'User data sources count:', len(details['datasets'])
+            print 'Models count:', details['models_cnt']
+            print '-'*50
+        return details
+
+    def create_project(self, title, description = '', task = 'bin_class'):
+        data={'hardware': 'cloud',
+                'scope': 'private',
+                'task': task,
+                'compute_now': 0,
+                'description': description,
+                'title':title}
+
+        print 'Create project with data', data
+
+        response = self._make_request(url_name = 'project', request_type = 'post', input_json = data)
+        print 'Response', response
+        if response.status_code == 201:
+            print 'Project successfully created'
+        details = self._get_data(response)
+        return details
+
+    def _get_s3_policy(self, project_hid):
+        print 'Get S3 Policy', project_hid
+        response = self._make_request(url_name = 's3policy', request_type = 'post', input_json = {'project_hid':project_hid})
+        print 'Response', response
+        details = self._get_data(response)
+        print 'Details', details, type(details)
+        return details
+
+    def _upload_file_to_s3(self, storage_url, access_key, file_policy, signature, dst_dataset_dir, file_path):
+        print 'Upload to S3'
+        data = {
+            'key': dst_dataset_dir + file_path,
+            'AWSAccessKeyId': access_key,
+            'acl': 'private',
+            'policy': file_policy,
+            'signature': signature,
+            'file': file_path
+        }
+        print 'Data', data
+        response = self._make_request(custom_url=storage_url, request_type = 'post', input_json = data, with_header=False)
+        print 'Response', response
+        #details = self._get_data(response)
+        #print 'Details', details, type(details)
+
+    def add_new_dataset(self, project_hid, title, file_path, prediction_only = False):
+        print 'Add new dataset', project_hid, title, file_path
+        policy = self._get_s3_policy(project_hid)
+        print 'Policy', policy
+        access_key = policy['access_key'];
+        file_policy = policy['train_policy'];
+        signature = policy['train_signature'];
+        dst_dataset_dir = policy['dst_dataset_dir'];
+        storage_url = policy['storage_url'];
+        print 'Upload file to S3'
+
+        self._upload_file_to_s3(storage_url, access_key, file_policy, signature, dst_dataset_dir, file_path)
--- mljar/main.py
+++ mljar/main.py
@@ -1,4 +1,13 @@
 from client import MljarClient
 
 mljar_client = MljarClient(token = 'your_token')
-mljar_client.get_projects()
+#mljar_client.get_projects()
+#mljar_client.get_project_details('YPVd39E43ODn')
+
+#mljar_client.create_project(title="MLJAR-API-2", description='Auto', task='bin_class')
+
+
+fname = '/home/piotr/webs/mljar/test/data/binary_part_iris_converted.csv'
+mljar_client.add_new_dataset(project_hid='pWovmM0N3YEy', title='train', file_path=fname)
+
+#mljar_client.get_projects()
