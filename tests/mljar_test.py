be37c12e43f363b8d53b04fd016eb01c3b0a466d
pp <pplonski86@gmail.com>
2016-12-15 14:36:46 +0100

    skeleton for tests

---
+++ tests/mljar_test.py
@@ -0,0 +1,14 @@
+'''
+    MLJAR unit tests.
+'''
+import os
+import unittest
+
+class TestMljar(unittest.TestCase):
+    def test_api(self):
+        self.assertTrue(True)
+        self.assertEqual(1,1)
+
+
+if __name__ == '__main__':
+    unittest.main()
