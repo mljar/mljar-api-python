9931f7c7abf6664323edcd84661339cdef02113d
pp <pplonski86@gmail.com>
2017-02-01 19:09:29 +0100

    add more tests

--- tests/run.py
+++ tests/run.py
@@ -5,10 +5,10 @@ import os
 import unittest
 
 from project_client_test import ProjectClientTest
-#from dataset_client_test import DatasetClientTest
-#from experiment_client_test import ExperimentClientTest
-#from result_client_test import ResultClientTest
-#from mljar_test import MljarTest
+from dataset_client_test import DatasetClientTest
+from experiment_client_test import ExperimentClientTest
+from result_client_test import ResultClientTest
+from mljar_test import MljarTest
 
 if __name__ == '__main__':
     unittest.main()
