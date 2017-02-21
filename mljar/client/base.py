5422d209da4e433bb39a08f17beb1392c592273b
pp <pplonski86@gmail.com>
2017-02-21 18:27:59 +0100

    cleaning

--- mljar/client/base.py
+++ mljar/client/base.py
@@ -43,7 +43,6 @@ class MljarHttpClient(object):
                 if response.status_code != 204:
                     logger.debug("Response content: {}, headers: {}".format(response.json(), response.headers))
             except Exception as e:
-                print 'Failed', request_url
                 logger.error("Request failed: {} {}".format(response.content, str(e)))
         self._check_response_status(response)
         return response
@@ -68,69 +67,3 @@ class MljarHttpClient(object):
                 raise BadRequestException()
             else:
                 response.raise_for_status()
-
-
-'''
-def _get_data(self, response):
-    if response is None:
-        return None
-
-    try:
-        data = response.json()
-    except ValueError as e:
-        raise JSONReadException('Get data failed, %s' % str(e) )
-
-    if not response.ok:
-        msg = [data[m] for m in ("id", "message") if m in data][1]
-        raise DataReadException(msg)
-
-    return data
-'''
-
-'''
-
-        self._urls = {
-            'project':      '/'.join([self.API_ENDPOINT, API_VERSION, 'projects']),
-            'dataset':      '/'.join([self.API_ENDPOINT, API_VERSION, 'datasets']),
-            'experiment':   '/'.join([self.API_ENDPOINT, API_VERSION, 'experiments']),
-            'result':       '/'.join([self.API_ENDPOINT, API_VERSION, 'results/']),
-            'predict':      '/'.join([self.API_ENDPOINT, API_VERSION, 'predict/']),
-            'predictions':  '/'.join([self.API_ENDPOINT, API_VERSION, 'predictions']), # it is not a bug, we don't need here '/'
-            'download_prediction': '/'.join([self.API_ENDPOINT, API_VERSION, 'download/prediction/']),
-            's3policy':     '/'.join([self.API_ENDPOINT, API_VERSION, 's3policy/']),
-            'accept_column_usage': '/'.join([self.API_ENDPOINT, API_VERSION, 'accept_column_usage/']),
-        }
-
-
-    def _make_request(self, url_name = '', custom_url = '', request_type = 'get', url_additional = '', input_json = {}, with_header = True):
-        try:
-            response = None
-            headers = {'Authorization': 'Token '+self.TOKEN } #'Content-Type': 'application/json'
-            my_url = ''
-            if url_name in self._urls:
-            if my_url == '':
-                raise Exception('Wrong URL address')
-            if url_additional != '':
-                my_url += url_additional
-
-            #print 'request', my_url, request_type, input_json, with_header, headers
-
-            if request_type == 'get':
-                response = requests.get(my_url, headers=headers)
-            elif request_type == 'post':
-                if with_header:
-                    response = requests.post(my_url, data=input_json, headers=headers)
-                else:
-                    response = requests.post(my_url, data=input_json)
-            elif request_type == 'put':
-                if with_header:
-                    print my_url, 'with header', input_json
-                    response = requests.put(my_url, data=input_json, headers=headers)
-                else:
-                    response = requests.put(my_url, data=input_json)
-
-        except MljarException as e:
-            print 'There was an error during API call, %s' % str(e)
-        finally:
-            return response
-'''
