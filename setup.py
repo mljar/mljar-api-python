14b4e1247142339987ccba838bcceee4bf1655cb
pp <pplonski86@gmail.com>
2018-04-10 00:47:14 +0200

    merge with v3 [ci skip]

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='mljar',
-    version='0.0.8',
+    version='0.0.9',
     description='Python wrapper over MLJAR API',
     long_description=long_description,
     url='https://github.com/mljar/mljar-api-python',
@@ -19,9 +19,9 @@ setup(
     license='Apache-2.0',
     packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
     install_requires=['requests', 'marshmallow'],
-    classifiers=(
+    classifiers=[
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.6'
-    ),
+    ]
 )
